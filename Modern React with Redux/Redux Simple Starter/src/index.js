168e635f3abea0c4ff513c23011f68b072cccdce
Jamie Coe <jamiecoe66@gmail.com>
2017-04-11 07:54:45 +0100

    Handling User Events

--- Modern React with Redux/Redux Simple Starter/src/index.js
+++ Modern React with Redux/Redux Simple Starter/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCfg1UFE6Ycgg9s9OW2n0_SVCMA16cwb-o';
+
 // Create a new component, which produces some HTML
-const App = () => <div>Hi!</div>;
+const App = () => {
+  return(
+    <div>
+      <SearchBar />
+    </div>
+  )
+}
 
 
 // Take this componenet's HTML ans put it in the DOM
