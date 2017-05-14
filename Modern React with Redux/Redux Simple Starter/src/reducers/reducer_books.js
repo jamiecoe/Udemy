e7c10e5c4cea6ec7aa266fbffb1bc5a3a34dac22
Jamie Coe <jamiecoe66@gmail.com>
2017-05-14 20:06:32 +0100

    Redux simple starter complete

--- Modern React with Redux/Redux Simple Starter/src/reducers/reducer_books.js
+++ Modern React with Redux/Redux Simple Starter/src/reducers/reducer_books.js
@@ -3,10 +3,9 @@
 
 export default function() {
   return [
-    { title: 'JS: The Good Parts'},
-    { title: 'Harry Potter'},
-    { title: 'The Dark Tower'},
-    { title: 'Eloquent Ruby'}
+    { title: 'JS: The Good Parts', pages: 50},
+    { title: 'Harry Potter', pages: 355},
+    { title: 'The Dark Tower', pages: 234},
+    { title: 'Eloquent Ruby', pages: 135}
   ]
 }
- 
