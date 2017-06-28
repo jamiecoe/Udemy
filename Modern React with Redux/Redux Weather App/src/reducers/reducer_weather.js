e3aca1c4d14ceaaa324187567138404fde0fa1f5
Jamie Coe <jamiecoe66@gmail.com>
2017-06-28 06:39:15 +0100

    Avoiding State Mutation in Reducers

--- Modern React with Redux/Redux Weather App/src/reducers/reducer_weather.js
+++ Modern React with Redux/Redux Weather App/src/reducers/reducer_weather.js
@@ -1,3 +1,12 @@
-export default function (state = null, action) {  
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function (state = [], action) {
+  swtich (action.type) {
+    case FETCH_WEATHER :
+      // To avoid state mutation, use state.concat()
+      // This adds old state values along with new action.payload.data into a new array
+      // ES6 syntax, same as saying return state.concat([action.payload.data]);
+      return [ action.payload.data, ...state];
+  }
   return state;
 }
