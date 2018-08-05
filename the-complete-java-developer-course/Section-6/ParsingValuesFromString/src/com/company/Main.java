190df8e0eb372da4358d80ec4e7b3a3436d1b1b0
Jamie Coe <jamiecoe66@gmail.com>
2018-08-05 20:53:54 +0100

    printSquareStar

--- the-complete-java-developer-course/Section-6/ParsingValuesFromString/src/com/company/Main.java
+++ the-complete-java-developer-course/Section-6/ParsingValuesFromString/src/com/company/Main.java
@@ -3,26 +3,34 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    String numberAsString = "2018";
+        String numberAsString = "2018";
 
-	    int number = Integer.parseInt(numberAsString);
-	    double doubleNumber = Double.parseDouble(numberAsString);
+        int number = Integer.parseInt(numberAsString);
+        double doubleNumber = Double.parseDouble(numberAsString);
 
-        System.out.println(canPack(2, 1, 5));
+        printSquareStar(8);
     }
 
-    public static boolean canPack(int bigCount, int smallCount, int goal) {
-        if (bigCount < 0 || smallCount < 0 || goal < 0) {
-            return false;
+    public static void printSquareStar(int number) {
+        if (number < 5) {
+            System.out.println("Invalid Value");
+        } else {
+            for (int i = 0; i < number; i++) {
+                for (int j = 0; j < number; j++) {
+                    if (i == j) {
+                        System.out.print('*');
+                    } else if (i == 0 || j == 0) {
+                        System.out.print('*');
+                    } else if (i == number - 1 || j == number - 1) {
+                        System.out.print('*');
+                    } else if (j == number - (i + 1)) {
+                        System.out.print('*');
+                    } else {
+                        System.out.print(' ');
+                    }
+                }
+                System.out.println();
+            }
         }
-
-        int bigCountKg = bigCount * 5;
-        int smallCountKg = smallCount;
-
-        if (bigCountKg % (goal - smallCountKg) == 0) {
-            return true;
-        }
-
-        return goal <= bigCountKg + smallCountKg;
     }
 }
