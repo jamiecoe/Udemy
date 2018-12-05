84dd256477ee11237b65cfb9cf3ee3c5cc82f28e
jamiecoe <jamiecoe66@gmail.com>
2018-12-05 09:24:28 +0000

    (complete-java) Adventure with exits

--- the-complete-java-developer-course/Section-12/Adventure/src/com/jamiecoe/Main.java
+++ the-complete-java-developer-course/Section-12/Adventure/src/com/jamiecoe/Main.java
@@ -1,8 +1,60 @@
 package com.jamiecoe;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
+    private static Map<Integer, Location> locations = new HashMap<>();
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        locations.put(0, new Location(0, "In front on computer"));
+        locations.put(1, new Location(1, "End of road"));
+        locations.put(2, new Location(2, "Top of hill"));
+        locations.put(3, new Location(3, "Inside a building"));
+        locations.put(4, new Location(4, "In a valley"));
+        locations.put(5, new Location(5, "In a forest"));
+
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+
+        locations.get(2).addExit("N", 5);
+
+        locations.get(3).addExit("W", 1);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+
+        int loc = 1;
+
+        while (true) {
+            System.out.println(locations.get(loc).getDescription());
+            if (loc == 0) {
+                break;
+            }
+
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.print("Available exits are ");
+            for (String exit : exits.keySet()) {
+                System.out.print(exit + ", ");
+            }
+            System.out.println();
+
+            String direction = scanner.nextLine().toUpperCase();
+
+            if (exits.containsKey(direction)) {
+                loc = exits.get(direction);
+
+            } else {
+                System.out.println("You cannot go in that direction");
+            }
+        }
     }
 }
