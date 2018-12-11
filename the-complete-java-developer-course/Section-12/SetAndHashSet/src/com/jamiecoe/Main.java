dc134b0e2ed93bb7b0c274af9d94992f6cfdad88
jamiecoe <jamiecoe66@gmail.com>
2018-12-11 19:40:32 +0000

    (complete-java) Sets and HashSets

---
+++ the-complete-java-developer-course/Section-12/SetAndHashSet/src/com/jamiecoe/Main.java
@@ -0,0 +1,110 @@
+package com.jamiecoe;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+// Sets are a collection of objects
+// Unlike Lists, they have no defined ordering AND cannot contain duplicates
+// There's no way to retrieve an item from a Set...
+// You can check if Set contains item, or iterate over a Set
+// But attempting for example to get 10th element isn't possible
+// Sets are useful because Operations on Sets are very fast
+
+
+public class Main {
+    private static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+    private static Set<HeavenlyBody> planets = new HashSet<>();
+
+    public static void main(String[] args) {
+        HeavenlyBody temp = new HeavenlyBody("Mercury", 88);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Venus", 225);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Earth", 365);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        HeavenlyBody tempMoon = new HeavenlyBody("Moon", 27);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon);
+
+        temp = new HeavenlyBody("Mars", 687);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        tempMoon = new HeavenlyBody("Deimos", 1.3);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is still Mars
+
+        tempMoon = new HeavenlyBody("Phobos", 0.3);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is still Mars
+
+        temp = new HeavenlyBody("Jupiter", 4332);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        tempMoon = new HeavenlyBody("Io", 1.8);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is still Jupiter
+
+        tempMoon = new HeavenlyBody("Europa", 3.5);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is still Jupiter
+
+        tempMoon = new HeavenlyBody("Ganymede", 7.1);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is still Jupiter
+
+        tempMoon = new HeavenlyBody("Callisto", 16.7);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is still Jupiter
+
+        temp = new HeavenlyBody("Saturn", 10759);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Uranus", 30660);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Neptune", 165);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Pluto", 248);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        System.out.println("Planets");
+        for(HeavenlyBody planet : planets) {
+            System.out.println("\t" + planet.getName());
+        }
+
+        HeavenlyBody body = solarSystem.get("Mars");
+        System.out.println("Moons of " + body.getName());
+        for(HeavenlyBody jupiterMoon: body.getSatellites()) {
+            System.out.println("\t" + jupiterMoon.getName());
+        }
+
+        // Let's say we want to get all the moons in one Set
+        // You can create a union of two or more Sets. containing all the elements of the Sets
+        // Each element of a union will still only appear once, even if it was present in multiple Sets
+        // You can generate a union of Sets using the addAll() method
+        Set<HeavenlyBody> moons = new HashSet<>();
+        for(HeavenlyBody planet : planets) {
+            moons.addAll(planet.getSatellites());
+        }
+
+        System.out.println("All Moons");
+        for(HeavenlyBody moon : moons) {
+            System.out.println("\t" + moon.getName());
+        }
+    }
+}
