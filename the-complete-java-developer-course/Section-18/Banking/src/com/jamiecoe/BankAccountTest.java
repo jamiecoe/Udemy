b72d6203d14448e2ea18c147cd42b9465d1fae27
Jamie Coe <jamiecoe66@gmail.com>
2019-01-13 14:45:01 +0000

    (complete-java) Parameterized tests

--- the-complete-java-developer-course/Section-18/Banking/src/com/jamiecoe/BankAccountTest.java
+++ the-complete-java-developer-course/Section-18/Banking/src/com/jamiecoe/BankAccountTest.java
@@ -1,41 +1,71 @@
 package com.jamiecoe;
 
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
+    private BankAccount bankAccount;
+    private static int count;
+
+    // This will run once, before any of the class test methods
+    // The name of the method doesn't matter, as long as you have the annotation
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("This executes before any test cases");
+        System.out.println("Count = " + count++);
+    }
+
+    // This will run before each test
+    @Before
+    public void setup() {
+        bankAccount = new BankAccount("Jamie", "Coe", 100.00, BankAccount.CHECKING);
+    }
+
     @Test
     public void deposit() {
-        BankAccount bankAccount = new BankAccount("Jamie", "Coe", 100.00, BankAccount.CHECKING);
         double balance = bankAccount.deposit(200.00, true);
         assertEquals(300.00, balance, 0);
     }
 
     @Test
     public void withdraw() {
-        BankAccount bankAccount = new BankAccount("Jamie", "Coe", 100.00, BankAccount.CHECKING);
+        double balance = bankAccount.withdraw(600.00, true);
+        assertEquals(-500.00, balance, 0);
+    }
 
+    // We add 'expected' to annotation to say we're expecting test to throw an IllegalArgumentException
+    @Test(expected = IllegalArgumentException.class)
+    public void withdraw_notBranch() throws Exception {
+        bankAccount.withdraw(600.00, false);
     }
 
     @Test
     public void getBalance_deposit() {
-        BankAccount bankAccount = new BankAccount("Jamie", "Coe", 100.00, BankAccount.CHECKING);
-        double balance = bankAccount.deposit(200.00, true);
+        bankAccount.deposit(200.00, true);
         assertEquals(300.00, bankAccount.getBalance(), 0);
     }
 
     @Test
     public void getBalance_withdraw() {
-        BankAccount bankAccount = new BankAccount("Jamie", "Coe", 100.00, BankAccount.CHECKING);
-        double balance = bankAccount.withdraw(50.00, true);
+        bankAccount.withdraw(50.00, true);
         assertEquals(50.00, bankAccount.getBalance(), 0);
     }
 
     @Test
     public void isChecking_true() {
-        BankAccount bankAccount = new BankAccount("Jamie", "Coe", 100.00, BankAccount.CHECKING);
         assertTrue(bankAccount.isChecking());
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("Count = " + count++);
+        System.out.println("This exectutes after all test cases");
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Count = " + count++);
     }
 }
--- the-complete-java-developer-course/Section-18/Banking/src/com/jamiecoe/Main.java
+++ the-complete-java-developer-course/Section-18/Banking/src/com/jamiecoe/Main.java
@@ -1,8 +1,18 @@
 package com.jamiecoe;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("The application is running");
+
+        System.out.println(Arrays.deepToString(new Object[][]{
+                {100.00, true, 1100.00},
+                {200.00, true, 1200.00},
+                {325.14, true, 1325.14},
+                {489.33, true, 1489.33},
+                {1000.00, true, 2000.00},
+        }));
     }
 }
