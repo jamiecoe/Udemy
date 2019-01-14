e917deac150922e8aeb0032f9c083dfe157ef0a3
Jamie Coe <jamiecoe66@gmail.com>
2019-01-14 17:44:58 +0000

    (complete-java) JUnit Challange continued

--- the-complete-java-developer-course/Section-18/JUnitChallenge/src/com/jamiecoe/Utilities.java
+++ the-complete-java-developer-course/Section-18/JUnitChallenge/src/com/jamiecoe/Utilities.java
@@ -17,4 +17,37 @@ public class Utilities {
 
         return stringBuilder.toString().toCharArray();
     }
+
+    // Remove pairs of the same character that are next to each other
+    // This is done by removing one occurence of the character.
+    // "ABBCDEEF" -> "ABCDEF"
+    // "ABCBDEEF" -> "ABCBDEF"
+    public String removePairs(String source){
+
+        if (source.length() < 2) {
+            return source;
+        }
+
+        char[] sourceLetters = source.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < sourceLetters.length - 1; i++) {
+            if (sourceLetters[i] != sourceLetters[i + 1]) {
+                stringBuilder.append(sourceLetters[i]);
+            }
+        }
+
+        stringBuilder.append(sourceLetters[sourceLetters.length - 1]);
+
+        return stringBuilder.toString();
+    }
+
+    // perform a conversion based on some internal business rule.
+    public int converter(int a, int b) {
+        return (a/b) + (a * 30) - 2;
+    }
+
+    public String nullIfOddLength(String source) {
+        return source.length() % 2 == 0 ? source : null;
+    }
 }
--- the-complete-java-developer-course/Section-18/JUnitChallenge/src/com/jamiecoe/UtilitiesTest.java
+++ the-complete-java-developer-course/Section-18/JUnitChallenge/src/com/jamiecoe/UtilitiesTest.java
@@ -1,28 +1,33 @@
 package com.jamiecoe;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class UtilitiesTest {
 
+    private Utilities utilities;
+
+    @Before
+    public void beforeEach() {
+        utilities = new Utilities();
+    }
+
     @Test
     public void everyNthChar_shouldReturnOriginalArrayIfLengthShorterThanN() {
-        Utilities utilities = new Utilities();
         char[] inputArray = new char[]{ 'a', 'b', 'c' };
         assertArrayEquals(inputArray, utilities.everyNthChar(inputArray, 4));
     }
 
     @Test
     public void everyNthChar_shouldReturnOriginalArrayIfNIs1() {
-        Utilities utilities = new Utilities();
         char[] inputArray = new char[]{ 'a', 'b', 'c' };
         assertArrayEquals(inputArray, utilities.everyNthChar(inputArray, 1));
     }
 
     @Test
     public void everyNthChar_shouldReturnNewArrayContainingEvery2ndCharacter() {
-        Utilities utilities = new Utilities();
         char[] inputArray = new char[]{ 'a', 'b', 'c', 'd' };
         char[] expectedArray = new char[]{ 'b', 'd' };
         assertArrayEquals(expectedArray, utilities.everyNthChar(inputArray, 2));
@@ -30,9 +35,54 @@ public class UtilitiesTest {
 
     @Test
     public void everyNthChar_shouldReturnNewArrayContainingEvery3rdCharacter() {
-        Utilities utilities = new Utilities();
         char[] inputArray = new char[]{ 'a', 'b', 'c' };
         char[] expectedArray = new char[]{ 'c' };
         assertArrayEquals(expectedArray, utilities.everyNthChar(inputArray, 3));
+    }
+
+    @Test
+    public void removePairs_shouldReturnSameStringIfLengthIsLessThan2() {
+        String inputString = "A";
+        assertEquals(inputString, utilities.removePairs(inputString));
+    }
+
+    @Test
+    public void removePairs_shouldReturnSameStringIfNoDuplicates() {
+        String inputString = "ABCDE";
+        assertEquals(inputString, utilities.removePairs(inputString));
+    }
+
+    @Test
+    public void removePairs_shouldRemoveDuplicatesNextToEachOther() {
+        String inputString = "EFGGHI";
+        String expectedString = "EFGHI";
+        assertEquals(expectedString, utilities.removePairs(inputString));
+    }
+
+    @Test
+    public void removePairs_shouldRemoveDuplicatesNextToEachOtherButKeepSeperatedDuplicates() {
+        String inputString = "JKKLMJ";
+        String expectedString = "JKLMJ";
+        assertEquals(expectedString, utilities.removePairs(inputString));
+    }
+
+    @Test
+    public void converter_shouldReturn300For10And5() {
+        assertEquals(300, utilities.converter(10, 5));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void converter_shouldThrowArithmeticExceptionIfBIs0() throws Exception {
+        utilities.converter(100, 0);
+    }
+
+    @Test
+    public void nullIfOddLength_shouldReturnNullIfStringHasOddLength() {
+        assertNull(utilities.nullIfOddLength("Hello"));
+    }
+
+    @Test
+    public void nullIfOddLength_shouldNotReturnNullIfStringHasEvenLength() {
+        assertNotNull(utilities.nullIfOddLength("Bell"));
     }
 }
