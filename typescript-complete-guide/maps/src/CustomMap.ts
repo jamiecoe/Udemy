27e1ca6a2e97017f92d78fc9aa0ffe8e63833c1d
Jamie Coe <jamiecoe66@gmail.com>
2020-05-15 08:19:50 +0100

    (complete-typescript) CustomMap

---
+++ typescript-complete-guide/maps/src/CustomMap.ts
@@ -0,0 +1,17 @@
+export class CustomMap {
+  private googleMap: google.maps.Map
+
+  constructor(elementId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(elementId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    })    
+  }
+
+  
+
+
+}
--- typescript-complete-guide/maps/src/index.ts
+++ typescript-complete-guide/maps/src/index.ts
@@ -1,9 +1,4 @@
 import { User } from './User'
+import { CustomMap } from './CustomMap'
 
-new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-})
+new CustomMap('map')
